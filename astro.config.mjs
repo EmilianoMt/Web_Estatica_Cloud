8b00e54e92be16d5ec9a5df4c4486424c4273863
Emiliano Portillo <emiliano.portillo1331@gmail.com>
2025-02-26 21:47:24 -0600

    Fix: Changes to deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://emilianomt.github.io',
+  base: 'Web_Estatica_Cloud',
   vite: {
     plugins: [tailwindcss()]
   }
